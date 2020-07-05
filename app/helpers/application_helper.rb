aab462b73e8eff37b2238773490d81edbc1abd4a
Steve Kim <dkim.steve@gmail.com>
2020-07-05 11:00:11 -0700

    added meta tag and removed filter category label

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,20 @@
 module ApplicationHelper
-    def show_errors(object, field_name)
-        if object.errors.any?
-          if !object.errors.messages[field_name].blank?
-            puts '-------'
-            puts object.errors.messages[field_name]
-            puts '-------'
+    # def show_errors(object, field_name)
+    #     if object.errors.any?
+    #       if !object.errors.messages[field_name].blank?
+    #         puts '-------'
+    #         puts object.errors.messages[field_name]
+    #         puts '-------'
             
-          end
-        end
-      end 
+    #       end
+    #     end
+    # end 
+
+    def title(title)
+      content_for(:title) { title }
+    end
+    
+    def meta_description(description)
+      content_for(:meta_description) { description }
+    end
 end
