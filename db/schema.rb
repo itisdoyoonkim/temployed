484b83cf94dc6cd3c869f72f4c9b4c206a1f1952
Steve Kim <dkim.steve@gmail.com>
2020-07-04 22:15:05 -0700

    2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_29_050518) do
+ActiveRecord::Schema.define(version: 2020_07_05_045943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 2020_06_29_050518) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.boolean "current"
     t.index ["user_id"], name: "index_experiences_on_user_id"
   end
 
